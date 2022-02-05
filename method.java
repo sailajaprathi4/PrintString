bc090c2019064aad97470f9f833d385c9b7badaf
sailajaprathi4 <98017006+sailajaprathi4@users.noreply.github.com>
2022-02-05 02:50:13 -0300

    PrintString Program

---
+++ method.java
@@ -0,0 +1,18 @@
+class method {
+
+//printString method
+static void printString()
+{
+	System.out.println("Hello Great Learning");
+}
+
+//main method
+public static void main(String[] args) {
+//create object of main class
+method myMethod = new method();
+
+myMethod.printString();
+//call printString method using Main class object.
+
+}
+}
